ef8f300c5082d9825a433bfdfcfe67d11f98ab95
Haniy <Haniy@HS>
2025-06-18 17:00:37 -0400

    Add project files.

---
+++ Chapter 2 Project/Chapter 2 Project.cpp
@@ -0,0 +1,23 @@
+// Chapter 2 Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double current, sum1, sum2, sum3;
+    
+    cout << "Whats the current level?: ";
+    cin >> current;
+
+    sum1 = (1.5 * 5) + current;
+    cout << "Ocean level after 5 years: " << sum1 << endl;
+
+    sum2 = (1.5 * 7) + current;
+    cout << "Ocean level after 7 years: " << sum2 << endl;
+
+    sum3 = (1.5 * 10) + current;
+    cout << "Ocean level after 10 years: " << sum3 << endl;
+}
+
