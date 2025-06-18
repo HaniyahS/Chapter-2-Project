f9d8fae7d03d3009b83d6d902ffc980484858b2f
HaniyahS <Lionroar8743@gmail.com>
2025-06-18 17:33:34 -0400

    more comments

--- Chapter 2 Project/Chapter 2 Project.cpp
+++ Chapter 2 Project/Chapter 2 Project.cpp
@@ -6,11 +6,14 @@ using namespace std;
 
 int main()
 {
+//Initializing variables
     double distance1, distance2;
 
+//Calculating distances
     distance1 = (20 * 23.5);
     distance2 = 20 * 28.9;
 
+//Printing out distances
     cout << "Distance on 1 full tank of gas in town: " << distance1 << "mi." << endl;
     cout << "Distance on 1 full tank of gas on the highway " << distance2 << "mi." << endl;
 
